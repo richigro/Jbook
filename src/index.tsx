a860a588b682b10e1b6bcf812187a72c1f6400a3
Ricardo Guerrero <ricardo.guerrero@economicmodeling.com>
2023-02-15 07:55:08 -0600

    created plugin to connect to unpkg

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as esbuild from "esbuild-wasm";
 import { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 
 const App = () => {
   const ref = useRef<any>(null);
@@ -23,12 +24,13 @@ const App = () => {
       console.log("wait up");
       return;
     }
-    const result = await ref.current.transform(input, {
-      loader: "jsx",
-      target: "es2015",
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()]
     });
-    setCode(result.code);
-    console.log("the transpiled code!", result);
+    setCode(result.outputFiles[0].text);
   };
   return (
     <div>
